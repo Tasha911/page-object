271ae92a1fdad4db59fcd618ee64ed4786af954e
“Natalia <fa.nat.ka911@gmail.com>
2026-04-28 15:49:17 +0700

    change Test - random amount

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,19 +1,21 @@
 package ru.netology.web.test;
 
 import com.codeborne.selenide.Selenide;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.web.data.DataHelper;
 import ru.netology.web.page.DashBoardPage;
 import ru.netology.web.page.LoginPage;
+
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static ru.netology.web.data.DataHelper.getAuthInfo;
 
 public class MoneyTransferTest {
 
-    @BeforeAll
-    static void beforeAll() {
+    @BeforeEach
+    void beforeEach() {
         // получаем из DataHelper логин и пароль (vasya/qwerty123)
         var info = getAuthInfo();
         // получаем из DataHelper секретный код для этого пользователя (12345)
@@ -28,7 +30,7 @@ public class MoneyTransferTest {
     }
 
     @Test
-        void shouldTransferMoneyPositive() {
+    void shouldTransferMoneyPositive() {
         var dashBoardPage = new DashBoardPage();
 
         // для перевода берем из DataHelper переменные с информацией о первой карте (номер с которого преводить)
@@ -38,8 +40,9 @@ public class MoneyTransferTest {
         var firstCardBalance = dashBoardPage.getCardBalance(0);
         var secondCardBalance = dashBoardPage.getCardBalance(1);
 
-        // позитивный тест - сумма меньше остатка
-        int amount = firstCardBalance / 2;
+        // позитивный тест - генерируем число от 0 до firstCardBalance - 1
+        var random = new Random();
+        int amount = random.nextInt(firstCardBalance);
 
         // вычисляем сколько должно остаться на первой карте
         var expectedBalanceFirstCard = firstCardBalance - amount;
@@ -58,27 +61,5 @@ public class MoneyTransferTest {
         // сравниваем с тем сколько должно было остаться (стр.40,42) с тем, что реально показал сайт
         assertEquals(expectedBalanceFirstCard, actualBalanceFirstCard);
         assertEquals(expectedBalanceSecondCard, actualBalanceSecondCard);
-    }
-
-    @Test
-    void shouldNotTransferIfAmountMoreThanBalance() {
-        var dashBoardPage = new DashBoardPage();
-        var firstCardInfo = DataHelper.getFirstCardInfo();
-
-        // вызываем метод из DashBoardPage для переменных баланса - считываем текущие балансы карт
-        var firstCardBalance = dashBoardPage.getCardBalance(0);
-        var secondCardBalance = dashBoardPage.getCardBalance(1);
-
-        // негативный тест - сумма больше остатка на 500
-        int amount = firstCardBalance + 500;
-
-        // нажимаем кнопку "Пополнить" у второй карты, открывается страница перевода - выполняем перевод на вторую карту
-        var transferPage = dashBoardPage.selectCardToTransfer(1);
-        // вводим сумму, номер первой карты и жмем "Пополнить"
-        dashBoardPage = transferPage.makeTransfer(String.valueOf(amount), firstCardInfo);
-
-        // проверяем, что балансы НЕ изменились
-        assertEquals(firstCardBalance, dashBoardPage.getCardBalance(0));
-        assertEquals(secondCardBalance, dashBoardPage.getCardBalance(1));
     }
 }
